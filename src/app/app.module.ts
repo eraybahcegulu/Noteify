ecf4172012d3aff5786d03d455b8c85158e7d2c8
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-11-16 23:05:35 +0300

    upd for nz module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,31 +10,20 @@ import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { TodoComponent } from './components/todo/todo.component';
-import { NzIconModule } from 'ng-zorro-antd/icon';
 import { HomeComponent } from './components/home/home.component';
 import { ShoppingComponent } from './components/shopping/shopping.component';
-import { NzCardModule } from 'ng-zorro-antd/card';
-import { NzTableModule } from 'ng-zorro-antd/table';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzEmptyModule } from 'ng-zorro-antd/empty';
-import { NzAlertModule } from 'ng-zorro-antd/alert';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoteComponent } from './components/note/note.component';
-
+import { NzModule } from './nz.module';
 @NgModule({
   declarations: [AppComponent, TodoComponent, HomeComponent, ShoppingComponent, NoteComponent],
   imports: [
     NgbModule,
     RouterModule,
     DragDropModule,
-    NzCardModule,
-    NzTableModule,
     BrowserModule,
     BrowserAnimationsModule,
-    NzInputModule,
-    NzIconModule,
-    NzEmptyModule,
-    NzAlertModule,
+    NzModule,
     FormsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
